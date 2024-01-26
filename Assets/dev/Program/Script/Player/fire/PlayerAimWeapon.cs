0ec35a69b2cfc0ad5c4578fddd9dcb72f9b09c9f
polachade nantaruan <96508766+Gripenyuki@users.noreply.github.com>
2024-01-26 19:26:09 +0700

    gun

---
+++ Assets/dev/Program/Script/Player/fire/PlayerAimWeapon.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+
+public class PlayerAimWeapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float bulletForce = 20f;
+
+    void Update()
+    {
+        Aim();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Aim()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.right * bulletForce, ForceMode2D.Impulse);
+    }
+}
