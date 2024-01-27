dc251df8ce444ddeae15348ee8dfa0650d8ac4e1
Tan20114 <156587199+Tan20114@users.noreply.github.com>
2024-01-27 11:08:32 +0700

    Player Sound

--- Assets/dev/Program/Script/Player/PlayerMovement.cs
+++ Assets/dev/Program/Script/Player/PlayerMovement.cs
@@ -12,18 +12,45 @@ public class PlayerMovement : MonoBehaviour
     public float fly = 5f;
     private bool isGrounded;
 
+    private AudioSource Source;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private AudioClip WalkSound;
+    [SerializeField] private AudioClip JumpSound;
 
+    void Start()
+    {
+        Source = GetComponent<AudioSource>();
+    }
+    
     // Update is called once per frame
     void Update()
     {   
         horizontal = Input.GetAxisRaw("Horizontal");
+        
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                Source.Play();
+            }
+            else if(Input.GetKeyUp(KeyCode.A))
+            {
+                Source.Stop();
+            }
+            if(Input.GetKeyDown(KeyCode.D))
+            {
+                Source.Play();
+            }
+            else if(Input.GetKeyUp(KeyCode.D))
+            {
+                Source.Stop();
+            }        
+
         isGrounded = IsGrounded ();
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Source.PlayOneShot(JumpSound);
             rb.bodyType = RigidbodyType2D.Kinematic;
             rb.velocity = new Vector2(rb.velocity.x, CalculateFlySpeed());
         }
